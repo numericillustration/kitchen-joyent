a086f7e50a0d29bf6b9f104d7ede9392968debc3
Michael Hicks <nooneofconsequence@gmail.com>
2015-12-18 21:44:19 -0800

    trying symlinking gcc

--- scripts/install-smartos-new.sh
+++ scripts/install-smartos-new.sh
@@ -1,6 +1,7 @@
 pkgin update
 pkgin -y install ruby-2.2.3
 pkgin -y install gcc49
+ln -s /opt/local/gcc49/bin/gcc /opt/local/bin
 pkgin -y install gmake
 gem install chef --version 12.0.3 --no-ri --no-rdoc
 gem install builder --no-ri --no-rdoc
